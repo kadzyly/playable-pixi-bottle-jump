19592f025a427c5b1b4c3b19c7aec2b51291f729
KadzyLy <kadzyly@gmail.com>
2026-01-22 16:38:18 +0100

    feat: import imposterData

--- src/core/Assets.ts
+++ src/core/Assets.ts
@@ -5,11 +5,26 @@ import shelfAsset from 'assets/shelf.png';
 import sofaAsset from 'assets/chair.png';
 import bgSegment from 'assets/BG_seg.png';
 
+import { imposterData } from '../data/imposterData';
+import imposterImage from 'assets/imposter/imposter.png';
+
 export async function loadAssets(): Promise<void> {
   await PIXI.Assets.load([
     { alias: 'bg', src: bgSegment },
     { alias: 'char', src: charAsset },
     { alias: 'shelf', src: shelfAsset },
-    { alias: 'sofa', src: sofaAsset }
+    { alias: 'sofa', src: sofaAsset },
+    { alias: 'imposterTexture', src: imposterImage }
   ]);
+
+  const texture = PIXI.Assets.get('imposterTexture');
+
+  const spritesheet = new PIXI.Spritesheet(
+    texture,
+    imposterData as any // Передаем наш объект
+  );
+
+  await spritesheet.parse();
+
+  PIXI.Cache.set('imposterSheet', spritesheet);
 }
